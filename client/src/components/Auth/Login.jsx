5600473d49051433a49c9bcadeecb6ff89475533
Aashutosh <aashutoshdahal22@gmail.com>
2024-12-24 07:03:52 +0545

    form using formik

---
+++ client/src/components/Auth/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      <p>Register component</p>
+    </div>
+  )
+}
+
+export default Login
---
+++ client/src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+      <p>Home component</p>
+      <Navbar />
+      <Hero />
+      <Footer />
+    </>
+  )
+}
+
+export default Home
